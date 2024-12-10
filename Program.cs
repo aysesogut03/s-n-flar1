d9fdabbcccf07b99cc2eff7e6851564df357e899
aysesogut03 <aysesogut2003@gmail.com>
2024-12-10 16:51:03 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ogrenci ogrenci1 = new ogrenci(1, "Ayse", "Sogut", 56, 64, 47, "BANÜ");
+
+            Console.WriteLine("UYGULAMAYA HOŞGELDİNİZ...");
+            Console.WriteLine("Yapılmak istenen işlemi seçiniz:");
+
+            islemleriGoster();
+
+            while (true)
+            {
+                string secim = Console.ReadLine();
+                switch (secim)
+                {
+                    case "1":
+                        ogrenci1.ogrenciBilgileriGoster();
+                        break;
+                   case "2":
+                        double ogrenciOrtalama=ogrenci1.ogrenciOrtalamasiBul();
+                        Console.WriteLine("Öğrencinin ortalaması:" + ogrenciOrtalama);
+                        break;
+                   case "3":
+                        ogrenci1.okulGetir();
+                        break;
+                   case "4":
+                        Console.WriteLine("Çıkış yap");
+                        break;
+
+                    default:
+                        Console.WriteLine("Böyle bir işlem yapamazsınız...");
+                        break;
+
+
+
+                }
+            }
+        }
+        static void islemleriGoster()
+        {
+            Console.WriteLine("1- öğrenci bilgileri göster.");
+            Console.WriteLine("2-öğrenci ortalaması göster.");
+            Console.WriteLine("3-öğrenci okulunu göster.");
+            Console.WriteLine("Çıkış yap");
+        }
+    }
+}
---
+++ ogrenci.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    public class ogrenci
+    {
+        private int ogrenciNo;
+        private string isim;
+        private string soyisim;
+        private int vize1;
+        private int vize2;
+        private int final;
+        private string okulİsmi;
+
+        public ogrenci(int ogrenciNo, string isim, string soyisim, int vize1, int vize2, int final, string okulİsmi)
+        {
+            this.ogrenciNo = ogrenciNo;
+            this.isim = isim;
+            this.soyisim = soyisim;
+            this.vize1 = vize1;
+            this.vize2 = vize2;
+            this.final = final;
+            this.okulİsmi = okulİsmi;
+        }
+    
+        public void ogrenciBilgileriGoster()
+        {
+            Console.WriteLine("öğrenci no:"+ogrenciNo);
+            Console.WriteLine("isim:"+isim);
+            Console.WriteLine("soyisim:"+soyisim);
+            Console.WriteLine("vize 1:"+vize1);
+            Console.WriteLine("vize 2:"+vize2);
+            Console.WriteLine("final:"+final);
+            Console.WriteLine("okul isim:"+okulİsmi);
+        }
+
+        public double ogrenciOrtalamasiBul()
+        {
+            double ortalama = 0.2 * vize1 + 0.2 * vize2 + 0.6 * final;
+            return ortalama;
+        }
+
+        public void okulGetir()
+        {
+            Console.WriteLine("Öğrenci okul ismi getir:" + okulİsmi);
+        }
+
+    }
+}
